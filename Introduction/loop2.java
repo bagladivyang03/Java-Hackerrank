0b246a4ee5265d85da5dfba857d906c827d11fa5
Divyang Bagla <bagladivyang03@gmail.com>
2020-08-04 21:46:23 +0530

    completed datatypes q

---
+++ Introduction/loop2.java
@@ -0,0 +1,38 @@
+/*
+A simple for loop program for geeting the givrn series for q visit :-https://www.hackerrank.com/challenges/java-loops/problem
+
+Sample Input
+2
+0 2 10
+5 3 5
+
+
+Sample Output
+2 6 14 30 62 126 254 510 1022 2046
+8 14 26 50 98
+
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+             int k = a;
+             
+            for(int j=0;j<n;j++){
+                k = k + (int) Math.pow(2,j)*b;
+                System.out.print(k + " ");
+            }
+            System.out.println("");
+        }
+        in.close();
+    }
+}
